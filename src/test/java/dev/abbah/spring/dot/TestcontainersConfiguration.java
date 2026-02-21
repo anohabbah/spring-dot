7c85d8bd710236e5aeac6385dcd6ce7edf5460d2
Abbah A <25776788+anohabbah@users.noreply.github.com>
2026-02-21 13:23:58 +0100

    chore: update testcontainers config

--- src/test/java/dev/abbah/spring/dot/TestcontainersConfiguration.java
+++ src/test/java/dev/abbah/spring/dot/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package dev.abbah.spring.dot;
 
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -7,12 +8,15 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
+
+  static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:18"));
 
   @Bean
+  @RestartScope
   @ServiceConnection
   PostgreSQLContainer postgresContainer() {
-    return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    return postgres;
   }
 
 }
